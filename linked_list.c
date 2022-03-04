bff6f6d948f42e662d167a98ffd88942d89a3071
Tommy Vercety <donvercety@users.noreply.github.com>
2022-03-04 19:14:35 +0200

    linked_list example added

---
+++ linked_list.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+void make_mode(node *node, int number);
+void list_show(node *list);
+void list_free(node *list);
+
+int main(void) {
+
+    // initialize list of size 0
+    node *list = malloc(sizeof(node));
+    make_mode(list, 1); 
+
+    // add number to list
+    node *n = malloc(sizeof(node));
+    make_mode(n, 2);
+    list->next = n;
+
+    // add number to list
+    n = malloc(sizeof(node));
+    make_mode(n, 3);
+    list->next->next = n;
+
+    list_show(list);
+    list_free(list);
+    return 0;
+}
+
+// ----------------------------------------------------------------------------
+// Helpers
+// ----------------------------------------------------------------------------
+
+void make_mode(node *node, int number) {
+    if (node == NULL) exit(1);
+
+    node->number = number;
+    node->next = NULL;
+}
+
+void list_show(node *list) {
+   for (node *tmp = list; tmp != NULL; tmp = tmp->next) {
+        printf("%i\n", tmp->number);
+    } 
+}
+
+void list_free(node *list) {
+    while (list != NULL) {
+        node *tmp = list->next;
+        free(list);
+        list = tmp;
+    }
+}
